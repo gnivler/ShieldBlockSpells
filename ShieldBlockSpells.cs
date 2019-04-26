b1219f1c0475ea24839fbf8f0cc0d54828d48e8b
gnivler <gnivil@gmail.com>
2019-04-26 16:10:00 -0600

    Initial commit

--- ShieldBlockSpells.cs
+++ ShieldBlockSpells.cs
@@ -1,6 +1,52 @@
-﻿namespace ShieldBlockSpells
+﻿using System.Reflection;
+using BepInEx;
+using Harmony;
+using UnityEngine;
+
+namespace ShieldBlockSpells
 {
-    public class Class1
+    [BepInPlugin("com.gnivler.ShieldBlockSpells", "ShieldBlockSpells", "1.0")]
+    public class ShieldBlockSpells : BaseUnityPlugin
     {
+        public void Awake()
+        {
+            var harmony = HarmonyInstance.Create("com.gnivler.ShieldBlockSpells");
+            //FileLog.Log("Startup");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        [HarmonyPatch(typeof(Character), "ReceiveHit", MethodType.Normal)]
+        [HarmonyPatch(new[]
+        {
+            typeof(Weapon),
+            typeof(DamageList),
+            typeof(Vector3),
+            typeof(Vector3),
+            typeof(float),
+            typeof(float),
+            typeof(Character),
+            typeof(float),
+            typeof(bool)
+        })]
+        public class ReceiveHitPatch
+        {
+            public static void Prefix(Character __instance, ref DamageList _damage)
+            {
+                for (var i = 0; i < _damage.Count; i++)
+                {
+                    if (__instance.ShieldEquipped &&
+                        __instance.Blocking &&
+                        _damage[i].Type == DamageType.Types.Fire ||
+                        _damage[i].Type == DamageType.Types.Frost ||
+                        _damage[i].Type == DamageType.Types.Decay ||
+                        _damage[i].Type == DamageType.Types.Electric ||
+                        _damage[i].Type == DamageType.Types.Ethereal)
+                    {
+                        //FileLog.Log($"{_damage[i]} is now 0");
+                        _damage[i].Damage = 0f;
+                    }
+                }
+            }
+        }
     }
 }
